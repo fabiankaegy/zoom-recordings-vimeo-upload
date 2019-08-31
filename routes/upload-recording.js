93f8f5ffa005ddb646e1b7cd56b53ba980e8d2a1
Fabian Kägy <fabian@arvernus.info>
2019-08-31 12:27:03 +0200

    return early

--- routes/upload-recording.js
+++ routes/upload-recording.js
@@ -17,6 +17,9 @@ router.post("/", async (request, response, next) => {
 	const video = recording_files.filter(
 		recording => recording.recording_type === "shared_screen_with_speaker_view"
 	)[0];
+
+	response.send(video);
+
 	try {
 		const filePath = await downloadVideo(video.download_url);
 		const result = await uploadToVimeo(toTimeString(video.recording_start));
